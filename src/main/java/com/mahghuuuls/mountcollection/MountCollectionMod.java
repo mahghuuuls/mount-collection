14579ca06a5a926dfbdf6cc5ff72204507823705
Mahghuuuls <37412309+mahghuuuls@users.noreply.github.com>
2026-08-25 11:10:19 -0300

    Initialize Mount Collection from ForgeDevEnv

---
+++ src/main/java/com/mahghuuuls/mountcollection/MountCollectionMod.java
@@ -0,0 +1,22 @@
+package com.mahghuuuls.mountcollection;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(
+        modid = Tags.MOD_ID,
+        name = Tags.MOD_NAME,
+        version = Tags.VERSION,
+        dependencies = "required-after:forge@[14.23.5.2847,)"
+)
+public final class MountCollectionMod {
+
+    public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        LOGGER.info("Initializing {}", Tags.MOD_NAME);
+    }
+}
